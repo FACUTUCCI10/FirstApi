39e593fac2f557aba48733df4a7e539b6af93a7d
facutucci10 <tuccifacundo473@gmail.com>
2025-08-31 15:45:06 -0300

    Agregar archivos de proyecto.

---
+++ FirstApi/Controllers/BooksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace FirstApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}
